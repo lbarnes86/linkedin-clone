81ed247362f2004bf426d236158697c657970e8c
L II <lii@Ls-MacBook-Pro.lan>
2021-11-09 02:06:53 -0600

    updated Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -29,6 +29,23 @@ const Main = (props) => {
                 </button>
             </div>
             </ShareBox>
+            <div>
+                <Article>
+                    <SharedActor>
+                        <a>
+                            <img src="/images/user.svg" alt="" />
+                            <div>
+                                <span>Title</span>
+                                <span>Info</span>
+                                <span>Date</span>
+                            </div>
+                        </a>
+                        <button>
+                            <img src="/images/ellipsis.svg" alt="" />
+                        </button>
+                    </SharedActor>
+                </Article>
+            </div>
         </Container>
 };
 
@@ -63,10 +80,114 @@ const ShareBox = styled(CommonCard)`
             min-height: 48px;
             background: transparent;
             border: none;
+            display: flex;
+            align-items: center;
+            font-weight: 600;
+
+        }
+        &:first-child {
+            display: flex;
+            align-items: center;
+            padding: 8px 16px 0px 16px;
+
+            img {
+                width: 48px;
+                border-radius: 50%;
+                margin-right: 8px;
+            }
+            button {
+                margin: 4px 0;
+                flex-grow: 1;
+                border-radius: 35px;
+                padding-left: 16px;
+                border: 1px solid rgba(0, 0, 0, 0.15);
+                background-color: white;
+                text-align: left;
+            }
+
+            
+        }
+        &:nth-child(2) {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+            padding-bottom: 4px;
+
+            button {
+                img {
+                    margin: 0 4px 0 -2px;
+                }
+                span {
+                    color: #70b5f9
+                }
+            }
         }
     }
 `
+const Article = styled(CommonCard)`
+    padding: 0;
+    margin: 0 0 8px;
+    overflow: visible;
 
 
+`
+
+const SharedActor = styled.div`
+    padding-right: 40px;
+    flex-wrap: nowrap;
+    padding: 12px 16px 0;
+    margin-bottom: 8px;
+    align-items: center;
+    display: flex;
+    a {
+        margin-right: 12px;
+        flex-grow: 1;
+        overflow: hidden;
+        display: flex;
+        text-align: none;
+
+        img {
+            width: 48px;
+            height: 48px;
+
+        }
+        & > div {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            flex-basis: 0;
+            margin-left: 8px;
+            overflow: hidden;
+
+            span {
+                text-align: left;
+
+                &:first-child {
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: rgba(0, 0, 0, 1);
+            
+                }
+                
+                &:nth-child(n + 1) {
+                    font-size: 12px;
+                    color: rgba(0, 0, 0, 0.6);
+                }
+            
+            }
+
+        }
+    }
+button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+
+}
+
+`
 
 export default Main;
