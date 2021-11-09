a6e55ea4ec59ba39a4b97415a9e0721502bca5c1
L II <lii@Ls-MacBook-Pro.lan>
2021-11-08 22:24:10 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Login from './components/Login';
+import Home from './components/Home';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login />
+          </Route>
+          <Route path='/home'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+
+     </Router>
+      
+    </div>
+  );
+}
+
+export default App;
